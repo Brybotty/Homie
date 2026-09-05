2dfe555b228de66e1aba7ffb5d582cfca7f9bc38
ProjectRyann <bryanlex2407@hotmail.com>
2026-09-05 15:24:36 -0500

    fix(prod): restaurar apiUrl a /api para evitar bloqueo de CORS mediante proxy de Vercel

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://homieshop-col-api-aah2e2hvexghh7bm.canadacentral-01.azurewebsites.net/api',
+  apiUrl: '/api',
   wompiPublicKey: 'pub_prod_841FO6vUXX5J3SPW9TB7Zm5qv06aWoSi',
   wompiPaymentLink: 'https://checkout.wompi.co/l/VPOS_I9xaK0',
   nequiPhoneNumber: '3206182526',
